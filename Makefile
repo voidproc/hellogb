14c692f7f7980b7826291647862f0196d78249c0
voidProc <voidproc@gmail.com>
2023-06-29 23:02:33 +0900

    Add files

---
+++ Makefile
@@ -0,0 +1,31 @@
+ROM := hellogb.gb
+ROM_NAME := hellogb
+ROM_TITLE := HELLOGB
+INC_DIR := inc
+BUILD_DIR := build
+
+ASM := rgbasm
+ASM_FLAGS := -i $(INC_DIR)/
+
+LINK := rgblink
+LINK_FLAGS := --sym $(BUILD_DIR)/$(ROM_NAME).sym
+
+FIX := rgbfix
+FIX_FLAGS := -t $(ROM_TITLE) -v -p 0
+
+SRC := main.s
+OBJ := $(addprefix $(BUILD_DIR)/, $(SRC:.s=.o))
+
+all: clean $(ROM)
+
+$(ROM): $(OBJ)
+	$(LINK) $(LINK_FLAGS) -o $(BUILD_DIR)/$@ $<
+	$(FIX) $(FIX_FLAGS) $(BUILD_DIR)/$@
+
+$(OBJ): $(SRC)
+	$(ASM) $(ASM_FLAGS) -o $@ $<
+
+clean:
+	rm -f $(BUILD_DIR)/*.sym
+	rm -f $(BUILD_DIR)/*.gb
+	rm -f $(BUILD_DIR)/*.o
---
+++ main.s
@@ -0,0 +1,200 @@
+include "hardware.inc"
+
+
+;-------------------------------------------------------------------------------
+; Wait for VBlank (LY=144)
+;-------------------------------------------------------------------------------
+
+macro wait_vblank
+.waitVBlank\@
+    ldh a, [rLY]
+    cp a, SCRN_Y
+    jr nz, .waitVBlank\@
+endm
+
+
+;-------------------------------------------------------------------------------
+; Interrupt (VBlank) $40
+;-------------------------------------------------------------------------------
+
+section "VBlank Int", rom0[$0040]
+    push af
+    push hl
+    call FadeInBG
+    ;call Scroll
+    pop hl
+    pop af
+    reti
+
+
+;-------------------------------------------------------------------------------
+; Interrupt (VBlank) $40
+;-------------------------------------------------------------------------------
+
+section "Header", rom0[$0100]
+    ; Disable interrupts while setup phase
+    di
+
+    jp EntryPoint
+
+    ; For cartridge header area (?)
+    ds $150 - @, 0
+
+
+;-------------------------------------------------------------------------------
+; Entry point $200
+;-------------------------------------------------------------------------------
+
+section "Main", rom0[$0200]
+EntryPoint:
+    ; Reset registers
+    xor a
+    ldh [rIF], a
+    ldh [rNR52], a
+    ldh [rSTAT], a
+    ldh [rSCY], a
+    ldh [rSCX], a
+    ldh [rLYC], a
+
+    ; Initial scroll pos
+    ld a, -4
+    ldh [rSCY], a
+
+    ; Initial palette
+    ld a, %00000000
+    ldh [rBGP], a
+
+    ; Init vars
+    ld a, 16
+    ldh [FadeInDelay], a
+    ld a, -1
+    ldh [FadeInPaletteIndex], a
+
+    ; Wait for VBlank
+    wait_vblank
+
+    ; Turn LCD off
+    xor a
+    ldh [rLCDC], a
+
+    ; Copy font tiledata
+    ld hl, $9000
+    ld de, FontTiles
+    ld bc, FontTilesEnd - FontTiles
+.copyFont
+    ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c  ; (b|c)!=0
+    jr nz, .copyFont
+
+    ; Setup tilemap
+    ld hl, $9903
+    ld de, HelloText
+.printHello
+    ld a, [de]
+    ld [hli], a
+    inc de
+    and a
+    jr nz, .printHello
+
+    ; Setup interrupts
+    ld a, IEF_VBLANK
+    ldh [rIE], a
+    ei
+
+    ; Turn LCD on
+    ld a, %10000001
+    ldh [rLCDC], a
+
+.loop
+    jr .loop
+
+
+;-------------------------------------------------------------------------------
+; BG Fade in
+;-------------------------------------------------------------------------------
+
+FadeInBG:
+    ; Decrement delay counter to 0
+    ld hl, FadeInDelay
+    dec [hl]
+    jr z, .updatePalette
+    ret
+
+.updatePalette
+    ; Reset delay counter
+    ld [hl], 16
+
+    ; Increment palette index
+    ldh a, [FadeInPaletteIndex]
+    inc a
+    cp a, 4
+    jr z, .exitFadeIn
+    ldh [FadeInPaletteIndex], a
+
+    ; Get palette[index]
+    ld hl, FadeInPalette
+    add a, l
+    ld l, a
+    ld a, [hl]
+
+    ; Set BGP
+    ldh [rBGP], a
+
+.exitFadeIn
+    ret
+
+
+;-------------------------------------------------------------------------------
+; Scroll
+;-------------------------------------------------------------------------------
+
+Scroll:
+    ld d, 6  ; delay counter
+.delay
+    dec d
+    jr nz, .delay
+    dec e
+    ld a, e
+    ldh [rSCX], a
+    ret
+
+
+;-------------------------------------------------------------------------------
+; Font tiledata definition
+;-------------------------------------------------------------------------------
+
+section "Tiles", rom0
+FontTiles:
+include "font.s"
+FontTilesEnd:
+
+
+;-------------------------------------------------------------------------------
+; Text
+;-------------------------------------------------------------------------------
+
+section "Text", rom0
+HelloText:
+    db "HELLO GAMEBOY!", 0
+
+
+;-------------------------------------------------------------------------------
+; BG Palette List (Fade in)
+;-------------------------------------------------------------------------------
+
+section "Palette", rom0
+FadeInPalette:
+    db %00000000, %01010100, %10100100, %11100100,
+
+
+;-------------------------------------------------------------------------------
+; Variables
+;-------------------------------------------------------------------------------
+
+section "Vars", hram
+FadeInDelay: ds 1
+FadeInPaletteIndex: ds 1
